40c45e902405837981e328c0c241a26b2e7e6f81
27100311 <27100311@lums.edu.pk>
2026-03-17 14:31:06 +0500

    add failing test for deletecity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -16,4 +16,19 @@ public class CustomListTest {
         assertTrue(list.hasCity(lahore));
         assertFalse(list.hasCity(karachi));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City lahore = new City("Lahore", "Punjab");
+        City karachi = new City("Karachi", "Sindh");
+
+        list.addCity(lahore);
+        list.addCity(karachi);
+
+        list.deleteCity(lahore);
+
+        assertFalse(list.hasCity(lahore));
+        assertTrue(list.hasCity(karachi));
+    }
 }
